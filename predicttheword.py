5b96eaf3af2d6227529aee5e7c5639a19bd44df3
teknik0 <76602770+teknik0@users.noreply.github.com>
2022-08-26 05:50:12 -0400

    Update predicttheword.py
    General improvements, no new features

--- predicttheword.py
+++ predicttheword.py
@@ -1,72 +1,84 @@
-import getpass
 import random
 import time
-#gets your username in order to find file
-user = getpass.getuser()
+import os
+import sys
 
-wordtxt = open("/Users/{}/Downloads/words1.txt".format(user), encoding='unicode_escape').read()
 
-#turns words.txt into a list (of words)
-list_of_words = wordtxt.splitlines()
-#turns list into a string
-def listToString(s): 
-    str1 = ""     
-    for ele in s: 
-        str1 += ele       
-    return str1
-#returns false instead of true if an underscore is in the list
-def listisnotunderscoresatall(e):
+def no_underscores(e):
     t=True
     for i in e:
         if i == "_":
             t=False
     return t
 
+def list_to_string(s): 
+    str1 = ""     
+    for ele in s: 
+        str1 += str(ele)       
+    return str1
 
 
-#:) random time between messages because i feel like it
-print("\nHello " + user + "!")
+while True:
+    try:
+        #finds words1.txt in current directory
+        with open(os.path.join(sys.path[0], "words1.txt"), "r") as f:
+            wordtxt = (f.read())
+    
+        global list_of_words
+        #turns words.txt into a list (of words)
+        list_of_words = wordtxt.splitlines()
+
+        break
+    
+    except:
+            #file not found message
+            print("Words file not found. \n Check if \"words1.txt\" is in the same directory as the .py file \n Retrying in 10 seconds")
+            time.sleep(10)
+            print("Retrying...")
+
+
+#random time between messages because i feel like it
+ee = bool(round(time.time())%2)
+print("\n" "Hello! " * ee + "Welcome!" * (not ee))
 time.sleep(random.randint(1, 3))
-print('\n\nthis game is called predict the word')
+print('\nThis game is called predict the word')
 time.sleep(random.randint(1, 3))
-print('you have to guess the word as it slowly appears on the screen')
+print('\nTry and guess the word as it slowly appears on the screen')
+time.sleep(random.randint(1, 3))
+print('\nTake too long, and you will lose')
 time.sleep(random.randint(1, 3))  
 
-#repeats game the amount of times input specifies
-for i in range(int(input("Ok, lets start! how many times would you like to play?(One play session is about half a minute)"))):
-    #takes a random word from list, lowercases it, and saves it to word variable
-    wod = random.choice(list_of_words).lower()
+
+#repeats game loop the amount of times input specifies
+for i in range(int(input("\nOk, lets start! how many times would you like to play? (One play session is about half a minute) "))):
+    #takes a random word from list and fixes it up
+    word = random.choice(list_of_words).lower().strip()
+    #generates shuffled numbers that are the same length as the word
+    #for the randomized revealing of letters
+    wordlen = len(word)
     
-    word=wod.strip()
+    randlen = list(range(wordlen))
+
+    random.shuffle(randlen)
     
-    wordlen=len(word)
-    
-    listword=list(word)
-    
-    randlistlen=list(range(wordlen))
-    
-    random.shuffle(randlistlen)
-    
-    underscores="_"*wordlen
-    #gets a list of underscores that gradually become the word
-    underscoreslist=list(underscores)
-    
-    print("Game "+str(i+1)+" Has Started!")
+    #gets a string of underscores that gradually become the word
+    underscores=list("_"*wordlen)
+    #+1 to compensate for range starting at 0
+    print(f"\nGame {i+1} Has Started!")
     
     time.sleep(random.randint(1, 5))
     
+    
+    #core game loop
     for o in range(wordlen):
-        #for each cycle changes an underscore in underscores list
-        #to a character that is in listword(word in list form)
-        underscoreslist[randlistlen[o]] = listword[randlistlen[o]]
+        #for each cycle changes a random underscore
+        #to the letter that is in the corresponding position
+        underscores[randlen[o]] = word[randlen[o]]
         
-        print(listToString(underscoreslist))
-        #if underscores list has no more underscores i.e. word fully uncovered
-        if listisnotunderscoresatall(underscoreslist):
+        print(list_to_string(underscores))
+        
+        #if word revealed
+        if no_underscores(underscores):
             print("Too late! You haven't guessed the word!")
         time.sleep(random.randint(1, 5))
-
-    
-
-
-
+print("GAME END")
